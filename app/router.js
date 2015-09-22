8c89c0af4204d49a562d1ed0b02443c254e6fb99
Ivan Lavriv <ilavriv@admins-mac-mini.lv.remit.se>
2015-09-22 22:11:51 +0300

    Added routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('trips', function() {});
+  this.route('reports', function() {});
+
+  this.route('sessions', function() {
+    this.route('login');
+  });
 });
 
 export default Router;
